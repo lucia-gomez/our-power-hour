55bb8859eaca7ae795620ab38925b2f6ed7714d2
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-03-06 15:59:16 -0500

    change disabled control btn appearance

--- src/components/ButtonIcon.js
+++ src/components/ButtonIcon.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import Header from '../styles/Header';
 
 const ButtonIconStyle = styled(Header)`
-  background: ${props => props.active ? undefined : props.theme.colors.text};
   cursor: pointer;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   font-size: ${props => props.size}px;
   margin: 0px;
+  opacity: ${props => props.active ? 1 : 0.4};
 
   @media only screen and (max-width: 576px) {
     font-size: ${props => props.size * 0.75}px;
@@ -18,8 +18,7 @@ const ButtonIcon = (props) => {
   return <ButtonIconStyle
     onClick={props.onClick}
     className="material-icons"
-    active={props.active}
-    size={props.size}
+    {...props}
   >
     {props.icon}
   </ButtonIconStyle>
