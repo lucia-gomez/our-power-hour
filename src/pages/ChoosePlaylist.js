11f1642993d6f53c03bc51dca927ac36eddc1848
lucia-gomez <isabel.lucia.gomez@gmail.com>
2021-03-29 14:50:49 -0400

    validate playlist and display title

--- src/pages/ChoosePlaylist.js
+++ src/pages/ChoosePlaylist.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import { useState } from 'react';
 import PageTemplate from './PageTemplate';
 import ButtonLink from '../components/ButtonLink';
@@ -7,21 +8,52 @@ import ReactPlayer from 'react-player';
 
 const ChoosePlaylist = (props) => {
   const [url, setURL] = useState("");
+  const [title, setTitle] = useState();
 
-  const checkPlaylistID = (text) => {
+  const getPlaylistID = (text) => {
+    console.log(ReactPlayer.canPlay(text));
     if (ReactPlayer.canPlay(text)) {
       let i = text.indexOf("list=") + "list=".length;
       let id = text.slice(i, text.length);
       if (id.includes("&index")) {
         id = id.substring(0, id.indexOf("&index"));
       }
+      return id;
+    }
+    return null;
+  }
 
-      setURL(id);
-      localStorage.setItem("powerHourPlaylistID", id);
-      props.setPlaylistID(id);
-    } else {
+  const validatePlaylistID = (id) => {
+    const onError = _ => {
+      console.log("error")
+      setTitle("");
       setURL("");
     }
+
+    $.ajax({
+      url: "https://www.googleapis.com/youtube/v3/playlists",
+      type: "get",
+      data: {
+        key: "AIzaSyCX4XsTjApcjNbPZ8pMXdG-VU4aQVnZsag",
+        part: "snippet",
+        id: id,
+        maxResults: 1,
+      },
+      success: data => {
+        console.log("success", data)
+        if (data.items.length > 0) {
+          const snippet = data.items[0].snippet;
+          setTitle(snippet.title);
+
+          setURL(id);
+          localStorage.setItem("powerHourPlaylistID", id);
+          props.setPlaylistID(id);
+        } else {
+          onError("")
+        }
+      },
+      error: onError,
+    });
   }
 
   const stepNum = Number(props.path.substring(1));
@@ -32,10 +64,14 @@ const ChoosePlaylist = (props) => {
       caption={`Enter the URL of a YouTube playlist`}
       step={stepNum}
     >
-      <Input onChange={x => {
-        checkPlaylistID(x.target.value);
-      }
-      } />
+      <div>
+        <Input onChange={x => {
+          const id = getPlaylistID(x.target.value);
+          id ? validatePlaylistID(id) : setURL("");
+        }
+        } />
+        <p>{title}</p>
+      </div>
       <ButtonLink
         to={"/" + (stepNum + 1)}
         text="Next"
