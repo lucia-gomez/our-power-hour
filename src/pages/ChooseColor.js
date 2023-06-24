246f76a27181f8a320682e99bf3a0e3fcfe305fe
lucia <51058748+lucia-gomez@users.noreply.github.com>
2023-06-23 18:21:52 -0700

    use google analytics

--- src/pages/ChooseColor.js
+++ src/pages/ChooseColor.js
@@ -1,74 +1,77 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import PageTemplate from './PageTemplate';
-import ScrollableGrid from '../styles/ScrollableGrid';
-import ButtonLink from '../components/ButtonLink';
-import themes from '../scripts/themes';
+import ButtonLink from "../components/ButtonLink";
+import PageTemplate from "./PageTemplate";
+import ScrollableGrid from "../styles/ScrollableGrid";
+import styled from "styled-components";
+import themes from "../scripts/themes";
+import { useState } from "react";
 
 const CIRCLE_SIZE = 90;
 
 const ColorScheme = styled.svg`
-  cursor: pointer;
-  height: ${CIRCLE_SIZE}px;
-  width: ${CIRCLE_SIZE}px;
-  margin-right: 15px;
-  margin-bottom: 20px;
+	cursor: pointer;
+	height: ${CIRCLE_SIZE}px;
+	width: ${CIRCLE_SIZE}px;
+	margin-right: 15px;
+	margin-bottom: 20px;
 `;
 
 const ColorCircle = styled.circle`
-  cx: ${CIRCLE_SIZE / 2};
-  cy: ${CIRCLE_SIZE / 2};
+	cx: ${CIRCLE_SIZE / 2};
+	cy: ${CIRCLE_SIZE / 2};
 `;
 
 const ChooseColor = (props) => {
-  const [selected, setSelected] = useState(localStorage.getItem("powerHourTheme") ?? Object.keys(themes)[0]);
+	const [selected, setSelected] = useState(
+		localStorage.getItem("powerHourTheme") ?? Object.keys(themes)[0]
+	);
 
-  const clickTheme = (themeName) => {
-    setSelected(themeName);
-    props.setTheme(themeName);
-    localStorage.setItem("powerHourTheme", themeName);
-    window.umami.trackEvent(themeName, "theme");
-  }
+	const clickTheme = (themeName) => {
+		setSelected(themeName);
+		props.setTheme(themeName);
+		localStorage.setItem("powerHourTheme", themeName);
+		window.gtag("event", "theme", {
+			themeName,
+		});
+	};
 
-  const defineGradient = theme => {
-    return (
-      <defs>
-        <linearGradient id={theme[0] + "Gradient"}>
-          {theme[1].gradientColors.map((color, i) => {
-            const offset = (100 / (theme[1].gradientColors.length - 1)) * i;
-            return <stop stopColor={color} offset={offset + "%"} key={i} />;
-          })}
-        </linearGradient>
-      </defs>
-    );
-  }
+	const defineGradient = (theme) => {
+		return (
+			<defs>
+				<linearGradient id={theme[0] + "Gradient"}>
+					{theme[1].gradientColors.map((color, i) => {
+						const offset = (100 / (theme[1].gradientColors.length - 1)) * i;
+						return <stop stopColor={color} offset={offset + "%"} key={i} />;
+					})}
+				</linearGradient>
+			</defs>
+		);
+	};
 
-  const stepNum = Number(props.path.substring(1));
+	const stepNum = Number(props.path.substring(1));
 
-  return (
-    <PageTemplate
-      title={"Step " + stepNum}
-      caption="Choose a color scheme (optional)"
-      path={props.path}
-      step={stepNum}
-    >
-      <p>{selected.replaceAll("-", " ")}</p>
-      <ScrollableGrid>
-        {Object.entries(themes).map((theme, i) =>
-          <ColorScheme onClick={() => clickTheme(theme[0])} key={i}>
-            {defineGradient(theme)}
-            <ColorCircle r={CIRCLE_SIZE / 2} fill={`url(#${theme[0]}Gradient)`} />
-            <ColorCircle r={(CIRCLE_SIZE / 2) * 0.8} fill={theme[1].bg} />
-          </ColorScheme>
-        )}
-      </ScrollableGrid>
-      <ButtonLink
-        enabled={"true"}
-        to="/drink"
-        text="Next"
-      />
-    </PageTemplate >
-  );
+	return (
+		<PageTemplate
+			title={"Step " + stepNum}
+			caption="Choose a color scheme (optional)"
+			path={props.path}
+			step={stepNum}
+		>
+			<p>{selected.replaceAll("-", " ")}</p>
+			<ScrollableGrid>
+				{Object.entries(themes).map((theme, i) => (
+					<ColorScheme onClick={() => clickTheme(theme[0])} key={i}>
+						{defineGradient(theme)}
+						<ColorCircle
+							r={CIRCLE_SIZE / 2}
+							fill={`url(#${theme[0]}Gradient)`}
+						/>
+						<ColorCircle r={(CIRCLE_SIZE / 2) * 0.8} fill={theme[1].bg} />
+					</ColorScheme>
+				))}
+			</ScrollableGrid>
+			<ButtonLink enabled={"true"} to="/drink" text="Next" />
+		</PageTemplate>
+	);
 };
 
 export default ChooseColor;
